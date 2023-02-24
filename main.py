57ceb0ab713da5644e51b2a640ebc61ccc5546b5
haziq8 <125853006+haziq8@users.noreply.github.com>
2023-02-24 16:14:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,3 +25,11 @@ headers = {'User-Agent': 'Iphone 14'}
 url2 = 'http://httpbin.org/headers'
 request_header = requests.get(url2, headers=headers)
 print(request_header.text)
+
+# Modify the Header user-agent to display "iPhone 14"
+headers = {'User-Agent': 'iPhone 14'}
+# Test against test site that output the requester user-agent
+#url2 = 'http://httpbin.org/headers'
+url2 = 'http://172.18.58.80/headers.php'
+request_header = requests.get(url2, headers=headers)
+print(request_header.text)
